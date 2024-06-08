5e4d36d583aa139a6866b6d71a4d6183fae53b09
Yao Hao Lai <ylai@student.42signapore.sg>
2024-06-08 15:25:22 +0800

    feat(printf): initial commit. added basic functions

---
+++ ft_printf.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ylai <ylai@student.42singapore.sg>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/08 13:42:24 by ylai              #+#    #+#             */
+/*   Updated: 2024/06/08 15:22:06 by ylai             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "printf.h"
+
+/**
+ * print character by character
+ * if encounter "%", take a look out for the flags
+ * '-' means
+ * '+' means
+ * ' ' means
+ * '#' means
+ * '0' means
+ * '.' means
+ * 
+ * if encounter %, return a struct with the
+*/
+
+int	ft_printf(const char *str, ...)
+{
+	va_list	ap;
+	
+	while (*str)
+	{
+		
+	}
+}
